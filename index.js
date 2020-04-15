73d03721c9f345e8ecc71557a97a51649a319da6
cclovr <t.protsun@gmail.com>
2020-04-15 20:42:37 +0300

    add access to project for user by username

--- index.js
+++ index.js
@@ -1,68 +1,89 @@
 const Telegraf = require('telegraf')
 const bot = new Telegraf('YOUR_TOKEN')
 const axios = require('axios')
+const Markup = require('telegraf/markup')
+const users = require('./users')
 
-bot.start((ctx) => ctx.reply('Введите ID проекта gitlab'))
+bot.start((ctx) => {
+  const username = ctx.message.from.username
+  const foundUser = users.filter(user => user.usernameData === username)
+  if (foundUser[0].projects.length > 1) {
+    showMessageForManyProject(ctx, foundUser)
+  } else {
+    showMessageForOneProject(ctx, foundUser)
+  }
+})
 
-bot.on('message', (ctx) => {
-  const idProject = ctx.message.text
-  const regexIdProject = new RegExp(/^[0-9]+$/)
-  const v = regexIdProject.test(idProject)
+function showMessageForOneProject (ctx, foundUser) {
+  const nameProject = foundUser[0].projects[0].fullName
+  getPipelines(ctx, nameProject)
+}
 
-  if (v) {
-    const projectPipelinesEndpoint = 'https://gitlab.indevlab.com/api/v4/projects/' + idProject.toString() + '/pipelines/?per_page=100'
-    axios.get(projectPipelinesEndpoint, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Private-Token': 'TOKEN_GITLAB'
-      }
-    }).then(res => {
-      const merges = res.data
-      const donePipelinesMerge = merges.filter(merge => merge.status === 'success')
-      if (!donePipelinesMerge.length) {
-        return ctx.reply('Не найдено успешных пайплайнов')
-      } else {
-        const groupBy = key => array =>
-          array.reduce((objectsByKeyValue, obj) => {
-            const value = obj[key]
-            objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj)
-            return objectsByKeyValue
-          }, {})
+function notFoundMessage (ctx) {
+  return ctx.reply('Не найдено успешных пайплайнов')
+}
 
-        const groupByTitle = groupBy('ref')
-        const groupByRef = groupByTitle(donePipelinesMerge)
-        const arrayObjectPipeline = Object.entries(groupByRef)
-        const l = arrayObjectPipeline.map(obj => {
-          return {
-            title: obj[1][0].ref
-          }
-        })
-        const p = []
-        p.push(l)
+function getPipelines (ctx, nameProject) {
+  const username = ctx.update.callback_query.from.username
+  const foundUser = users.filter(user => user.usernameData === username)
+  const foundProject = foundUser[0].projects.filter(project => project.fullName === nameProject)
+  const projectPipelinesEndpoint = foundProject[0].endpoint
+  axios.get(projectPipelinesEndpoint, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Private-Token': 'YOUR_TOKEN'
+    }
+  }).then(res => {
+    const merges = res.data
+    const donePipelinesMerge = merges.filter(merge => merge.status === 'success')
+    if (!donePipelinesMerge.length) {
+      /* if success pipeline was not found */
+      notFoundMessage(ctx)
+    } else {
+      /* if success pipeline was found */
+      const groupBy = key => array =>
+        donePipelinesMerge.reduce((objectsByKeyValue, obj) => {
+          const value = obj[key]
+          objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj)
+          return objectsByKeyValue
+        }, {})
 
-        p[0].map(pipeline => {
-          const num = pipeline.title.substring(0, pipeline.title.indexOf('/'))
-          const firstWordTask = num.substring(0, num.indexOf('-'))
-          function generateUrl () {
-            if (firstWordTask === 'BM') {
-              return 'dev.btm.idl.local/' + num + '/index.html'
-            } else if (firstWordTask === 'DOF') {
-              return 'doc-dev.telemed.care/' + num + '/analyses'
-            }
-          }
-          const result = generateUrl()
-          return ctx.reply('<a href="' + `${result}` + `">${pipeline.title}</a>`, { parse_mode: 'HTML' })
-        })
-      }
-    })
-      .catch(error => {
-        if (error.toString() === 'Error: Request failed with status code 404') {
-          return ctx.reply('Проект с таким ID не найден')
+      const groupByTitle = groupBy('ref')
+      const groupByRef = groupByTitle(donePipelinesMerge)
+      const arrayObjectPipeline = Object.entries(groupByRef)
+      const l = arrayObjectPipeline.map(obj => {
+        return {
+          title: obj[1][0].ref
         }
       })
-  } else {
-    ctx.reply('ID должен состоять из цифр')
-  }
+      const p = []
+      p.push(l)
+
+      p[0].map(pipeline => {
+        const num = pipeline.title.substring(0, pipeline.title.indexOf('/'))
+        const result = foundProject[0].link + num + '/index.html'
+        console.log(result)
+        return ctx.reply('<a href="' + `${result}` + `">${pipeline.title}</a>`, { parse_mode: 'HTML' })
+      })
+    }
+  })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+// show all projects for user with many project
+function showMessageForManyProject (ctx, foundUser) {
+  return ctx.reply('Выберите проект', {
+    ...Markup.inlineKeyboard([
+      foundUser[0].projects.map(project => Markup.callbackButton('' + project.fullName, project.fullName))
+    ]).extra()
+  })
+}
+
+bot.on('callback_query', (ctx) => {
+  const nameProject = ctx.update.callback_query.data
+  getPipelines(ctx, nameProject)
 })
 
 bot.launch()
---
+++ users.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    usernameData: 'cclovr',
+    projects: [
+      {
+        fullName: 'Betmatch',
+        shortName: 'BM',
+        link: 'dev.btm.idl.local/',
+        endpoint: 'https://gitlab.indevlab.com/api/v4/projects/139/pipelines/?per_page=100',
+        id: '139'
+      },
+      {
+        fullName: 'Doctor Online',
+        name: 'DOF',
+        link: 'doc-dev.telemed.care/',
+        id: '199',
+        endpoint: 'https://gitlab.indevlab.com/api/v4/projects/199/pipelines/?per_page=100',
+      },
+      {
+        fullName: 'My xata',
+        name: 'MX',
+        link: 'doc-dev.telemed.care/',
+        id: '199',
+        endpoint: 'https://gitlab.indevlab.com/api/v4/projects/199/pipelines/?per_page=100'
+      }
+    ]
+  },
+  {
+    usernameData: 'rooter',
+    projects: [
+      {
+        fullName: 'Betmatch',
+        name: 'BM',
+        link: 'dev.btm.idl.local/',
+        endpoint: 'https://gitlab.indevlab.com/api/v4/projects/139/pipelines/?per_page=100',
+        id: '139'
+      },
+      {
+        fullName: 'Doctor Online',
+        name: 'DOF',
+        link: 'doc-dev.telemed.care/',
+        id: '199',
+        endpoint: 'https://gitlab.indevlab.com/api/v4/projects/199/pipelines/?per_page=100',
+      }
+    ]
+  }
+]
